0952982803e8d0d4fe7abdbc34cef666d1d4b4d5
lauleehong <imlauleehong@gmail.com>
2021-06-07 14:19:56 +0800

    working iwdg prototype on single thread

--- src/threads/behavior_manager_thread.cpp
+++ src/threads/behavior_manager_thread.cpp
@@ -1,7 +1,10 @@
 #include <logging/log.h>
 LOG_MODULE_REGISTER(behavior_manager_thread, LOG_LEVEL_DBG);
 
+#include <zephyr.h>
+#include <device.h>
 #include <drivers/gpio.h>
+#include <drivers/watchdog.h>
 #include "threads.h"
 
 #define LED0_NODE DT_ALIAS(led0)
@@ -17,6 +20,19 @@ LOG_MODULE_REGISTER(behavior_manager_thread, LOG_LEVEL_DBG);
 #define LED2   DT_GPIO_LABEL(LED2_NODE, gpios)
 #define PIN2   DT_GPIO_PIN(LED2_NODE, gpios)
 #define FLAGS2 DT_GPIO_FLAGS(LED2_NODE, gpios)
+
+#if DT_HAS_COMPAT_STATUS_OKAY(st_stm32_watchdog)
+#define WDT_NODE DT_INST(0, st_stm32_watchdog)
+#endif
+#ifndef WDT_MAX_WINDOW
+#define WDT_MAX_WINDOW 5000U
+#endif
+#ifdef WDT_NODE
+#define WDT_DEV_NAME DT_LABEL(WDT_NODE)
+#else
+#define WDT_DEV_NAME ""
+#error "Unsupported SoC and no watchdog0 alias in zephyr.dts"
+#endif
 
 void execute_behavior_manager_thread(void)
 {
@@ -39,20 +55,47 @@ void execute_behavior_manager_thread(void)
 	int flags_arr[] = { FLAGS0, FLAGS1, FLAGS2 };
 	int current_led_id = 0;
 	for (int i = 0; i < 3; i++) {
-		ret = gpio_pin_configure(led_arr[i], pin_arr[i],
-					 GPIO_OUTPUT_ACTIVE | flags_arr[i]);
+		ret = gpio_pin_configure(led_arr[i], pin_arr[i], GPIO_OUTPUT_ACTIVE | flags_arr[i]);
 		if (ret < 0) {
 			return;
 		}
 	}
 
+	/* Initialization of Watchdog components in main.cpp */
+	int rc, wdt_channel_id;
+	const struct device* wdt;
+
+	wdt = device_get_binding(WDT_DEV_NAME);
+	if (!wdt) {
+		LOG_ERR("Cannot get WDT device");
+		return;
+	}
+	wdt_timeout_cfg wdt_config = wdt_timeout_cfg();
+	wdt_config.flags = WDT_FLAG_RESET_SOC;
+	wdt_config.window.min = 0U;
+	wdt_config.window.max = WDT_MAX_WINDOW;
+
+	wdt_channel_id = wdt_install_timeout(wdt, &wdt_config);
+	if (wdt_channel_id < 0) {
+		LOG_ERR("Watchdog install error %d", wdt_channel_id);
+		return;
+	}
+	rc = wdt_setup(wdt, WDT_OPT_PAUSE_HALTED_BY_DBG);
+	if (rc < 0) {
+		LOG_ERR("Watchdog setup error %d", rc);
+		return;
+	}
+	else {
+		LOG_INF("Watchdog successfully setup");
+	}
+
 	while (true) {
 		/* Thread Logic */
-		gpio_pin_set(led_arr[current_led_id], pin_arr[current_led_id],
-			     (int)led_is_on[current_led_id]);
+		gpio_pin_set(led_arr[current_led_id], pin_arr[current_led_id], (int)led_is_on[current_led_id]);
 		led_is_on[current_led_id] = !led_is_on[current_led_id];
 		current_led_id = (current_led_id + 1) % 3;
 
+		wdt_feed(wdt, wdt_channel_id);
 		k_msleep(sleep_time_ms);
 	}
 }
